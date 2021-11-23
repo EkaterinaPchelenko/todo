d5ac583a694eea9f08e0444db2e442128a80c42b
ekaterina_pchelenko <e.pchelenko@gmail.com>
2021-11-23 18:55:39 +0500

    added page with users

---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import UserList from "./components/User";
+
+
+class App extends React.Component{
+  constructor(props) {
+    super();
+    this.state = {
+      'users': []
+    }
+  }
+
+  componentDidMount() {
+    const users = [
+      {'username': 'Kate',
+      'first_name': 'Ekaterina',
+      'last_name': 'Pchelenko',
+      'email': 'kate@mail.com',
+      },
+      {'username': 'Ivan',
+      'first_name': 'Ivan',
+      'last_name': 'Ivanov',
+      'email': 'ivan@mail.com',
+      },
+    ]
+
+    this.setState(
+        {
+          'users': users
+        }
+    )
+  }
+
+  render()
+  {
+    return (
+      <div>
+        <UserList users={this.state.users}/>
+      </div>
+    );
+  }
+}
+
+export default App;
