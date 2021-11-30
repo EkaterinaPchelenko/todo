a2779c9d31877e17ee633f90c44c32713aa338b9
ekaterina_pchelenko <e.pchelenko@gmail.com>
2021-11-30 21:14:52 +0500

    added pagination

--- todo/settings.py
+++ todo/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'users',
     'project',
     'corsheaders',
+    'django_filters'
 
 ]
 
@@ -136,9 +137,12 @@ AUTH_USER_MODEL = 'users.User'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# REST_FRAMEWORK = {
+REST_FRAMEWORK = {
 #     'DEFAULT_RENDERER_CLASSES': [
 #         'rest_framework.renderers.JSONRenderer',
 #         'rest_framework.renderers.BrowsableAPIRenderer',
 #     ]
-# }
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
