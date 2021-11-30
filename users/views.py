58cb9e02ce9bd6ba025c33831f8d1651f5e7e424
ekaterina_pchelenko <e.pchelenko@gmail.com>
2021-11-30 21:27:25 +0500

    updated Users view

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import ListModelMixin, UpdateModelMixin
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from .models import User
 from .serializers import UserModelSerializer
 
 
-class UserModelViewSet(ModelViewSet):
+class UserCustomViewSet(ListModelMixin, UpdateModelMixin, GenericViewSet):
    queryset = User.objects.all()
    serializer_class = UserModelSerializer
+   renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
