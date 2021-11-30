e1a20a245a9c7bd2746ff8d5ea2b9887a48cf98c
ekaterina_pchelenko <e.pchelenko@gmail.com>
2021-11-30 21:41:15 +0500

    updated Projects view

--- project/views.py
+++ project/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render
+from django_filters.rest_framework import FilterSet, CharFilter
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 
 from project.models import Project, ToDo
 from project.serializers import ProjectModelSerializer, ToDoModelSerializer
 
 
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+   default_limit = 10
+
+
+class ProjectFilter(FilterSet):
+   name = CharFilter(lookup_expr='contains')
+
+   class Meta:
+       model = Project
+       fields = ['name']
+
+
 class ProjectModelViewSet(ModelViewSet):
    queryset = Project.objects.all()
    serializer_class = ProjectModelSerializer
+   pagination_class = ProjectLimitOffsetPagination
+   filterset_class = ProjectFilter
 
 
 class ToDoModelViewSet(ModelViewSet):
    queryset = ToDo.objects.all()
    serializer_class = ToDoModelSerializer
+
+
+
 
 # Create your views here.
